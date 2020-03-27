8b543367e581d7642ff9f07c8fd4a1f62469c4b3
hectortav <tavhector@gmail.com>
2020-03-27 17:59:03 +0200

    Add example

--- example/bloom_filter_example.dart
+++ example/bloom_filter_example.dart
@@ -1,6 +1,34 @@
 import 'package:bloom_filter/bloom_filter.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var bloom = BloomFilter(10000, 3);
+
+  bloom.add('hello world');
+  bloom.add(1234567890.toString());
+  bloom.add('{"first-name": "Dart", "last-name": "lang"}');
+
+  if (bloom.check('hello world') == true) {
+    print('"hello world" PROBABLY exists...');
+  }
+  else {
+    print('"hello world" DEFINITELY does not exist...');
+  }
+  if (bloom.check(1234567890.toString()) == true) {
+    print('"1234567890" PROBABLY exists...');
+  }
+  else {
+    print('"1234567890" DEFINITELY does not exist...');
+  }
+  if (bloom.check('{"first-name": "Dart", "last-name": "lang"}') == true) {
+    print('"{"first-name": "Dart", "last-name": "lang"}" PROBABLY exists...');
+  }
+  else {
+    print('"{"first-name": "Dart", "last-name": "lang"}" DEFINITELY does not exist...');
+  }
+  if (bloom.check('{"first-name": "Go", "last-name": "lang"}') == true) {
+    print('"{"first-name": "Go", "last-name": "lang"}" PROBABLY exists...');
+  }
+  else {
+    print('"{"first-name": "Go", "last-name": "lang"}" DEFINITELY does not exist...');
+  }
 }
